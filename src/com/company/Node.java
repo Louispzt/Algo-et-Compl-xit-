108430e466b848539c1a034ad48b5d0f7499935a
Louis <louispeizerat@gmail.com>
2020-03-03 23:47:21 +0100

    neighboor added

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import org.w3c.dom.NamedNodeMap;
+
+import javax.annotation.processing.SupportedSourceVersion;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,17 +10,21 @@ public class Node {
     String str;
     int value;
     Map<Node, Integer> neighboors;
+    Node nodeMaxVal;
+    int maxval = 0;
 
     public Node(String str) {
         this.str = str;
         value = 1;
         neighboors = new HashMap<>();
+        nodeMaxVal = this;
     }
 
     public void addNeihboor(Node neighboor){
-        if (neighboor.getNeighboor().containsKey(str)){
-            System.out.println("oui");
+        if (neighboors.get(neighboor) != null){
             int n = neighboors.get(neighboor);
+            if (n + 1 > maxval )
+                nodeMaxVal = neighboor;
             neighboors.replace(neighboor, n, n + 1);
         }
         else{
@@ -25,12 +32,10 @@ public class Node {
         }
     }
 
+    public Node getMaxNeighboor() {return nodeMaxVal; }
+
     public String getString(){
         return str;
-    }
-
-    public Map getNeighboor(){
-        return neighboors;
     }
 
     public void increaseValue() { value++; }
